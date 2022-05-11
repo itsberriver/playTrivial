793d3c4dcc5187bc8fdf6141e955604110a3b6d3
JessiJess <jessicamejiaosorio@AirdeJessiJess.home>
2022-05-11 15:46:44 +0200

    added question view  and its route

--- Desktop/trivialPlay/src/router/index.js
+++ Desktop/trivialPlay/src/router/index.js
@@ -10,6 +10,11 @@ const router = createRouter({
       component: HomeView
     },
     {
+      path: '/question',
+      name: 'question',
+      component: () => import('../views/QuestionView.vue')
+    },
+    {
       path: '/category',
       name: 'category',
       // route level code-splitting
