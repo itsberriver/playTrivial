5ea83b9d14f53db259631f81a8bbfdf0902c6bf6
JessiJess <jessicamejiaosorio@AirdeJessiJess.home>
2022-05-11 16:04:24 +0200

    modified home view

--- Desktop/trivialPlay/src/components/TheWelcome.vue
+++ Desktop/trivialPlay/src/components/TheWelcome.vue
@@ -3,21 +3,21 @@
 </script>
 
 <template >
-    <div class="text-white font-Guy text-7xl">
-      <p>TRIVUEAL</p>
-      <p>FACTÓRIA</p>
+    <div class="flex flex-col text-white mt-60 text-center font-Guy ">
+      <p class="text-6xl px-6">TRIVUEAL</p>
+      <p class="text-7xl">FACTÓRIA</p>
     </div>
 
 
-    <div class="text-white">
-      <button class="bg-[#8BD048] box-border h-10 w-60 rounded-3xl font-Mont"> 
+    <div class="text-white flex justify-center shadow-2xl text-center">
+      <button class="bg-[#8BD048] box-border h-10 w-40 rounded-3xl font-Mont mt-20" > 
         <RouterLink to="/category">START</RouterLink>
       </button>
     </div>
 
-    <div class="text-white">
+    <div class="text-white flex flex-col justify-center mt-60 text-center">
         <p>don’t know how to play?</p>
-        <p>PRESS HERE</p>
+        <p class="">PRESS HERE</p>
     </div>
 
 </template>
