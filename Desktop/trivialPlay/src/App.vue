56768c367838d0e87783a5614b9a5eb28f4dc5aa
JessiJess <jessicamejiaosorio@AirdeJessiJess.home>
2022-05-11 14:38:42 +0200

    added the correct routes and changed naming

--- Desktop/trivialPlay/src/App.vue
+++ Desktop/trivialPlay/src/App.vue
@@ -1,21 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Category from '@/components/Category.vue'
 
 </script>
 
 <template >
   <header>
   </header>
-    <main> 
-      <!-- <HelloWorld msg="You did it!" /> -->
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-        
-      <div id="background">
-        <!-- <RouterLink to="/category">START</RouterLink> -->
-      </div>
-    </main> 
+    
   <RouterView />
 </template>
 
---
+++ Desktop/trivialPlay/src/views/CategoryView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Category from '@/components/Category.vue'
+</script>
+
+<template>
+  <main>
+    <Category/>
+    <RouterLink to="/category"></RouterLink>
+  </main>
+</template>
